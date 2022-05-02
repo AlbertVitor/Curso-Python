5c9514bb248ed5eac23167e75f39085d5805af2c
Vitor <albertvitor18@outlook.com>
2022-05-02 13:21:33 -0300

    Update ex002.py

--- Python 3 - Módulo 1/Seção 01/ex002.py
+++ Python 3 - Módulo 1/Seção 01/ex002.py
@@ -1,3 +1,5 @@
+#Crie um programa que solicite o nome do usuarío e mostre na tela com uma mensagem de boas vindas
+
 nome = input('Digite seu nome: ')
 
 print(f'Olá {nome}, Bem vindo ao curso de Python 3!')
