a098ce88ec17c83d8c7b074e2b0aef5726605a71
Vitor <albertvitor18@outlook.com>
2022-05-03 14:12:22 -0300

    Exclui a seção 1

--- Python 3 - Módulo 1/Seção 01/Desafio-01.py
+++
@@ -1,3 +0,0 @@
-nome = input('Qual é seu nome? ')
-
-print('Olá', nome ,'! Bem vindo ao curso de Python 3!')
--- Python 3 - Módulo 1/Seção 01/Desafio-03.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Digite o primeiro numero: '))
-n2 = int(input('Digite o segundo numero: '))
-
-soma = n1 + n2
-
-print('A soma é:', soma)
-
