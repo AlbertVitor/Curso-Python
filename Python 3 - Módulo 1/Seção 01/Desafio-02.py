a5c87cfec2ef48d20bff0d27b67d654ba6f50243
Vitor <albertvitor18@outlook.com>
2022-05-03 14:13:44 -0300

    Adicionei a seção 1 com todo o conteúdo dela

---
+++ Python 3 - Módulo 1/Seção 01/Desafio-02.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+dia = input('Digite o dia de seu nascimento: ')
+mes = input('Digite o mes de seu nascimento: ')
+ano = input('Digite o ano de seu nascimento: ')
+
+print('Nome:',nome, '- nasceu no dia', dia ,'de', mes ,'de', ano ,'- Correto?')
---
+++ Python 3 - Módulo 1/Seção 01/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+
+print(f'Olá {nome}, Bem vindo ao curso de Python 3!')
